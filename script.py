d9c028a29c23878ddfb88e82252f9ed2ee36204e
Marek Palán <marek_palan@seznam.cz>
2022-02-08 00:40:25 +0100

    Doplněný sranda program

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
 print("Hello Marek")                    # První kód
+nalada = input("Máš se dnes dobře/špatně?").lower()
+if nalada == "dobře":
+    print("Gratuluji, k dobrému pocitu!")
+elif nalada == "špatně":
+    print("Nikdo Tě nemá rád, všichni Tě nenávidí...Usměj se, čuráku!")
+else:
+    print("Nauč se odpovídat přesně, ty vokurko!")
